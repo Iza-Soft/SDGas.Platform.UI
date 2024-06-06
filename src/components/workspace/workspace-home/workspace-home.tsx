72c3ee505ff6127ff1df9283463228be115b8032
IlkoAdamov <ilko.z.adamov@gmail.com>
2024-06-07 02:34:09 +0300

    Creating home page, adding cards

--- src/components/workspace/workspace-home/workspace-home.tsx
+++ src/components/workspace/workspace-home/workspace-home.tsx
@@ -6,28 +6,15 @@ const WorkspaceHome = (): React.JSX.Element => {
   return (
     <div className={style.container}>
       <div className={style.wrapper}>
-        {/* <div
-          style={{
-            border: "1px solid black",
-
-            color: "#0E2E5C",
-            width: "200px",
-            textAlign: "center",
-          }}
-        >
-          <h2>Why Us</h2>
-          <Divider color="#0E2E5C" />
-        </div> */}
-        <div
-          style={{ display: "flex ", border: "0px solid black", width: "100%" }}
-        >
-          <div style={{ width: "60%" }}>
+        <div style={{ border: "0px solid black", width: "100%" }}>
+          <div style={{ width: "100%" }}>
             <div
               style={{
                 border: "0px solid black",
                 color: "#0E2E5C",
                 width: "200px",
                 textAlign: "center",
+                margin: "auto",
               }}
             >
               <h2>Why Us</h2>
@@ -42,36 +29,80 @@ const WorkspaceHome = (): React.JSX.Element => {
               </p>
             </div>
           </div>
-          <div style={{ width: "40%" }}>
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              border: "0px solid black",
+              marginTop: "100px",
+            }}
+          >
             <div
               style={{
-                border: "0px solid black",
-                color: "#0E2E5C",
-                width: "200px",
-                textAlign: "center",
-                marginLeft: "auto",
+                width: "20%",
+                height: "200px",
+                backgroundColor: "#EFEFEF",
+                borderRadius: "70px 0px 0px 70px",
               }}
-            >
-              <h2>Contact Us</h2>
-              <Divider color="#0E2E5C" />
-            </div>
+            ></div>
             <div
-              className="textarea"
               style={{
-                paddingTop: "20px",
-                border: "0px solid black",
-                width: "65%",
-                marginLeft: "auto",
+                width: "80%",
+                height: "200px",
+                backgroundColor: "#EFEFEF",
+                borderRadius: "0px 70px 70px 0px",
               }}
-            >
-              <p>
-                Bennochy Road, Kirkcaldy, KY25HD,
-                <br />
-                United Kingdom
-              </p>
-              <p>ssdgasservices@gmail.com</p>
-              <p>0748 096 9210</p>
-            </div>
+            ></div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              border: "0px solid black",
+              marginTop: "100px",
+            }}
+          >
+            <div
+              style={{
+                width: "20%",
+                height: "200px",
+                backgroundColor: "#EFEFEF",
+                borderRadius: "70px 0px 0px 70px",
+              }}
+            ></div>
+            <div
+              style={{
+                width: "80%",
+                height: "200px",
+                backgroundColor: "#EFEFEF",
+                borderRadius: "0px 70px 70px 0px",
+              }}
+            ></div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              border: "0px solid black",
+              marginTop: "100px",
+            }}
+          >
+            <div
+              style={{
+                width: "20%",
+                height: "200px",
+                backgroundColor: "#EFEFEF",
+                borderRadius: "70px 0px 0px 70px",
+              }}
+            ></div>
+            <div
+              style={{
+                width: "80%",
+                height: "200px",
+                backgroundColor: "#EFEFEF",
+                borderRadius: "0px 70px 70px 0px",
+              }}
+            ></div>
           </div>
         </div>
       </div>
