8b5834d5d8515167879500965481619da47b8450
i.adamov <i.adamov@dynamosoftware.com>
2024-08-20 10:39:38 +0300

    adding page under construction

--- src/components/workspace/workspace-not-found/worksapce-not-found.tsx
+++ src/components/workspace/workspace-not-found/worksapce-not-found.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import style from "./style.module.css";
+import underConstruction from "../../../images/under-construction.jpg";
 
 const WorkspaceNotFound = (): React.JSX.Element => {
   return (
     <div className={style.container}>
-      <div className={style.wrapper}>Not Found</div>
+      <div className={style.wrapper}>
+        <div className={`${style.panel} row`}>
+          <img
+            src={underConstruction}
+            width="750"
+            alt="Under Construction"
+            className={style.pic}
+          />
+        </div>
+      </div>
     </div>
   );
 };
